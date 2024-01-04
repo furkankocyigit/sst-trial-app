684b5cc73b3331b860b8531d31e18e4d693a1778
Furkan <furkankocyigit58@gmail.com>
2024-01-04 11:07:23 +0300

    packages directory is remived

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from 'sst/node/api';
-
-export const handler = ApiHandler(async (_evt) => {
-    return {
-        statusCode: 200,
-        body: `Created by SST console ${new Date().toISOString()}`,
-    };
-});
